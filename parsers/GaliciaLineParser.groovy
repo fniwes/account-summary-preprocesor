d82d0f21dd825794f9e86a20c5130ce285f1459a
Fernando Niwes <fernando@soluciones3f.com.ar>
2015-05-02 23:15:07 -0300

    Save amount as number type

--- parsers/GaliciaLineParser.groovy
+++ parsers/GaliciaLineParser.groovy
@@ -3,7 +3,7 @@ package parsers
 class GaliciaLineParser implements LineParser {
     HashMap parse(String line) {
         def description = line[31..80].trim()
-        def amount = line[-10..-1].trim()
+        def amount = parseAmount(line[-10..-1])
         def currency = description.contains("USD") ? "USD" : "ARS"
         
         def date = line[7..8].trim()
@@ -13,5 +13,25 @@ class GaliciaLineParser implements LineParser {
 
         def result = [description: description, amount: amount, currency: currency, date: lastDate.clone()]
         return result;
+    }
+
+    float parseAmount( String amount ) {
+        String strAmount = amount.trim()
+        def isNegative = strAmount[-1..-1] == "-"
+
+        if(isNegative) strAmount = strAmount[0..-2]
+        strAmount = strAmount.replace(".", "")
+        strAmount = strAmount.replace(",", ".")
+
+        float floatAmount = 0
+        try {
+            floatAmount = strAmount.toFloat()
+        }
+        catch(Exception) {
+            System.err.println("An amount is not a number. Value: ${amount}")
+        }
+        if(isNegative) floatAmount *= -1
+
+        return floatAmount.round(2)
     }
 }
